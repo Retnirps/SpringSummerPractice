66c086c55f3d91b12ac1ce0c94c128a9b9578f2b
Retnirps <Vasiliy23898@gmail.com>
2020-07-08 19:25:52 +0300

    documentation added

--- src/main/java/com/example/TestServer/controllers/UserController.java
+++ src/main/java/com/example/TestServer/controllers/UserController.java
@@ -8,20 +8,29 @@ import com.example.TestServer.models.StatisticsModel;
 import com.example.TestServer.models.Triple;
 import com.example.TestServer.models.UserModel;
 import com.example.TestServer.services.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/data")
+@Api(value = "UserController - catch all url linking with user")
 public class UserController {
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/users")
+    @ApiOperation(value = "Add User")
     public Map addUser(@RequestBody UserDto userDto) {
         String username = userDto.getUsername();
         String email = userDto.getEmail();
@@ -30,6 +39,7 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
+    @ApiOperation(value = "Get User by some Id")
     public UserDto getUserById(@PathVariable(value = "id") Long userId)
     throws ResourceNotFoundException {
         UserModel userModel = this.userService.getUserById(userId);
@@ -40,6 +50,7 @@ public class UserController {
     }
 
     @PutMapping("/users")
+    @ApiOperation(value = "Set Online/Offline Status for User with Id")
     public UserStatusDto setStatusForId(@RequestBody UserStatusDto userStatusDto) {
         long userId = userStatusDto.getUserId();
         int statusId = userStatusDto.getStatus().equals("OFFLINE") ? 0 : 1;
@@ -49,6 +60,7 @@ public class UserController {
     }
 
     @GetMapping("/statistics")
+    @ApiOperation(value = "Get Statistics of Changing Statuses After some Timestamp")
     public Triple<List<StatisticsDto>> getAllUsersChangedStatusAfterTimestamp(@RequestBody StatisticsDto statisticsDto) {
         String status = statisticsDto.getStatus();
         long timestamp = statisticsDto.getTimestamp();
